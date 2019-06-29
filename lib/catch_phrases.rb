5eddc4172426b7568efde555c5ae5c302824b79b
Patrick Harrison <jahman@bellatlantic.net>
2019-06-29 01:47:13 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,7 +7,8 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
-def  link ="its Dnagerous to GO alone! take this"
+def  link 
+  link ="its Dnagerous to GO alone! take this"
   puts link
 end
 def any_phrase
