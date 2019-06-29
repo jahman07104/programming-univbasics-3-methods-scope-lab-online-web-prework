7ddd7a3f5bbe441731a894a1f749f2ebe6a85759
Patrick Harrison <jahman@bellatlantic.net>
2019-06-29 01:45:40 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,13 +1,14 @@
 def mario
-   phrase="it's-a me Mario"
+   phrase="it's-a me, Mario"
   
   puts phrase
  end
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
-
-  toadstool="its Dnagerous to GO alone! take this"
+end
+def  link ="its Dnagerous to GO alone! take this"
+  puts link
 end
 def any_phrase
   phrase= a barrel roll
